cc966bfccb0f161844ff0e4ee64da19808716e52
Dolaus <dsadasdasdas19@gmail.com>
2023-05-24 14:49:37 +0300

    fix

--- Lab-8/script.js
+++ Lab-8/script.js
@@ -1,6 +1,5 @@
 (function(){
   let names = ["Bill", "John", "Jen", "Jason", "Paul", "Frank", "Steven", "Larry", "Paula", "Laura", "Jim"];
-
   for (let i=0;i<names.length;i++) {
     if (names[i].toLowerCase().charAt(0)=='j') {
         SpeakGoodbye.speak(names[i]);    
@@ -8,8 +7,12 @@
       SpeakHello.speak(names[i]);
     }
   }
-  
- console.log("Name ends 'l' or 'k'");
+})();
+
+
+(function(){
+  let names = ["Bill", "John", "Jen", "Jason", "Paul", "Frank", "Steven", "Larry", "Paula", "Laura", "Jim"];
+  console.log("All names end 'l' or 'k'");
   for (const item of names) {
     if (item.toLowerCase().charAt(item.length-1)=='l'||
         item.toLowerCase().charAt(item.length-1)=='k') 
